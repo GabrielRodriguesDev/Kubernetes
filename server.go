32724cf5d174c2ac6302d41c4a64469771fc3b7b
gabriel.rodrigues.mota <gabriel.rodrigues.mota@outlook.com>
2022-08-02 22:39:54 -0300

    Evoluindo com o Kubernetes

--- server.go
+++ server.go
@@ -1,12 +1,16 @@
 package main
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 func main() {
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":80", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Salve Gabriel</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
 }
